9b2f4aee2db27d961d4b5ba833f80f74a2f43e56
kihong choi <khchoi0301@gmail.com>
2018-08-27 13:05:09 +0900

    Complete offLineMinimum.js

--- offLineMinimum/offLineMinimum.js
+++ offLineMinimum/offLineMinimum.js
@@ -1,6 +1,36 @@
+var subset=[];
+
 function offLineMinimum(strArr) {
-  // YOUR CODE HERE
-  return
+  subset=[];
+  var testArr=strArr;
+  (function iter(testArr){
+    console.log('start',testArr,testArr.length)
+    for(var i=0;i<testArr.length;i++){
+      if(testArr[i]==='E'){        
+        testArr.splice(i,1);
+        testArr.splice(minimum(testArr,i),1);
+        //console.log(testArr)
+        iter(testArr);
+      } 
+    }
+    console.log('finish')
+  })(testArr)   
+  console.log(subset.join(','));
+  return subset.join(',');
+}
+
+function minimum(testArr,num){
+  var minNum=testArr[0];
+  var minNumpos=0;  
+
+  for(i=0;i<num;i++){
+    if(minNum>testArr[i]){
+      minNum=testArr[i];
+      minNumpos=i;
+    }    
+  } 
+  subset.push(minNum);
+  return minNumpos;
 }
 
 offLineMinimum(["1","2","E","E","3"]); // => '1,2'
