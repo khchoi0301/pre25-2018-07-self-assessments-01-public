fc197ab7d673c2a00216b0b038649428952bd7d0
kihong choi <khchoi0301@gmail.com>
2018-08-27 12:07:16 +0900

    Complete hammingDistance

--- hammingDistance/hammingDistance.js
+++ hammingDistance/hammingDistance.js
@@ -1,6 +1,15 @@
 function hammingDistance(strArr) {
-  // YOUR CODE HERE
-  return
+  var arr1=strArr[0].split('');
+  var arr2=strArr[1].split('');
+  var difAlph=0;
+  console.log(arr1,arr2);
+  for(i=0;i<arr1.length;i++){
+    if(arr1[i]!==arr2[i]){
+      difAlph++;
+    }
+  }
+  //console.log(difAlph);
+  return difAlph
 }
 
 hammingDistance(["coder", "codec"]); // => 1
