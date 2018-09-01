b1892c4e7624707dd4a708eaa4e7740a907abf52
kihong choi <khchoi0301@gmail.com>
2018-09-01 19:42:22 +0900

    Replace subfunctions position

--- offLineMinimum/offLineMinimum.js
+++ offLineMinimum/offLineMinimum.js
@@ -17,21 +17,23 @@ function offLineMinimum(strArr) {
   })(testArr)   
   //console.log(subset.join(','));
   return subset.join(',');
+
+  function minimum(testArr,num){
+    var minNum=testArr[0];
+    var minNumpos=0;  
+  
+    for(i=0;i<num;i++){
+      if(minNum>testArr[i]){
+        minNum=testArr[i];
+        minNumpos=i;
+      }    
+    } 
+    subset.push(minNum);
+    return minNumpos; 
+  }
 }
 
-function minimum(testArr,num){
-  var minNum=testArr[0];
-  var minNumpos=0;  
 
-  for(i=0;i<num;i++){
-    if(minNum>testArr[i]){
-      minNum=testArr[i];
-      minNumpos=i;
-    }    
-  } 
-  subset.push(minNum);
-  return minNumpos;
-}
 
 offLineMinimum(["1","2","E","E","3"]); // => '1,2'
 offLineMinimum(["4","E","1","E","2","E","3","E"]); // => '4,1,2,3'
