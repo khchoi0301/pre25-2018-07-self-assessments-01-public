87582b34b84098dca6ac20a7d320c85714aac593
kihong choi <khchoi0301@gmail.com>
2018-09-02 15:15:06 +0900

    Replace var postion

--- offLineMinimum/offLineMinimum.js
+++ offLineMinimum/offLineMinimum.js
@@ -1,21 +1,18 @@
-var subset=[];
+
 
 function offLineMinimum(strArr) {
-  subset=[];
+  var subset=[];
   var testArr=strArr;
   (function iter(testArr){
-    //console.log('start',testArr,testArr.length)
     for(var i=0;i<testArr.length;i++){
       if(testArr[i]==='E'){        
         testArr.splice(i,1);
         testArr.splice(minimum(testArr,i),1);
-        //console.log(testArr)
         iter(testArr);
       } 
     }
-    //console.log('finish')
   })(testArr)   
-  //console.log(subset.join(','));
+
   return subset.join(',');
 
   function minimum(testArr,num){
@@ -33,7 +30,6 @@ function offLineMinimum(strArr) {
   }
 }
 
-
-
 offLineMinimum(["1","2","E","E","3"]); // => '1,2'
 offLineMinimum(["4","E","1","E","2","E","3","E"]); // => '4,1,2,3'
+offLineMinimum(["3","E","21","E","1","2","E","E"]); // 3,21,1,2
